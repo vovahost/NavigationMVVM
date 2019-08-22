2890a7e97fe868c251c5a97a0fbebdd3da607dca
Vlad <vlos@nodesagency.com>
2019-08-22 15:41:18 +0100

    Removed "open val" modifier.

--- app/src/main/java/com/example/navigation_mvvm/navigation/AppRoute.kt
+++ app/src/main/java/com/example/navigation_mvvm/navigation/AppRoute.kt
@@ -3,22 +3,22 @@ package com.example.navigation_mvvm.navigation
 import androidx.annotation.IdRes
 import com.example.navigation_mvvm.R
 
-sealed class RouteSection(@IdRes open val graph: Int) {
+sealed class RouteSection(@IdRes val graph: Int) {
     object Login : RouteSection(R.id.loginNavGraph)
     object Settings : RouteSection(R.id.settingsNavGraph)
 }
 
-sealed class RouteDestination(@IdRes open val destination: Int) {
+sealed class RouteDestination(@IdRes val destination: Int) {
 
     object Back : RouteDestination(-1)
 
-    sealed class Login(@IdRes override val destination: Int) : RouteDestination(destination) {
+    sealed class Login(@IdRes destination: Int) : RouteDestination(destination) {
 
         object Credentials : Login(R.id.credentialsFragment)
         object TermsConditions : Login(R.id.termsConditionsFragment)
     }
 
-    sealed class Settings(@IdRes override val destination: Int) : RouteDestination(destination) {
+    sealed class Settings(@IdRes destination: Int) : RouteDestination(destination) {
 
         object Profile : Settings(R.id.profileFragment)
         object Notifications : Settings(R.id.notificationsFragment)
